0e8fe7120d7a0bf220d96f30354789c56e0204b3
Regan <sooboy@foxmail.com>
2023-01-17 01:20:48 +0800

    change

--- src/example/tick.ts
+++ src/example/tick.ts
@@ -1,6 +1,7 @@
+import { filter, scan } from 'rxjs';
 import { SendMDMsg } from './../helper/wxMsg';
 import dayjs from 'dayjs';
-import { bufferCount } from 'rxjs';
+import { bufferCount, bufferTime, map } from 'rxjs';
 import { curry } from 'ramda'
 
 import { Log } from '../utils/log';
@@ -12,10 +13,10 @@ import { TickHub, isBig, zhenfu, isUpDownConfig } from '../eastmoney/tick';
 const { QueryStockTick, DelStockTick, HasStockTick, AddStockTick } = TickHub()
 
 export const codes = [
-    "688670", // 金迪克
+    // "688670", // 金迪克
     "000815", // 美利云
-    "002469", // 三维化学
-    "300505"  // 川金诺
+    // "002469", // 三维化学
+    // "300505"  // 川金诺
 ]
 
 const codeName = ( code: string ) =>
@@ -30,7 +31,7 @@ const codeName = ( code: string ) =>
     }
 }
 
-const bot = (code:string) => 
+const bot = ( code: string ) => 
 {
     switch ( code )
     {
@@ -71,11 +72,11 @@ const isInTime = ( time: string ) =>
 
 }
 
-const SendWrap = ( sender: ( msg: string, bot?: string ) => void ) => ( time: string, msg: string,bot?:string ) =>
+const SendWrap = ( sender: ( msg: string, bot?: string ) => void ) => ( time: string, msg: string, bot?: string ) =>
 {
     if ( isWorkingTime() && isInTime( time ) )
     {
-        sender( msg,bot )
+        sender( msg, bot )
     }
 }
 
@@ -94,68 +95,68 @@ const tick$ = codes.map( code => ( {
 
 
 // 异动与大单
-tick$.map( it =>
-{
-    const { code, data$ } = it;
-    const UpDown = ( count: number ) => ( fn: {
-        up: ( rate: number ) => boolean
-        down: ( rate: number ) => boolean
-    } ) =>
-    {
+// tick$.map( it =>
+// {
+//     const { code, data$ } = it;
+//     const UpDown = ( count: number ) => ( fn: {
+//         up: ( rate: number ) => boolean
+//         down: ( rate: number ) => boolean
+//     } ) =>
+//     {
 
-        data$?.pipe(
-            bufferCount( count, 1 ),
-        ).subscribe( its =>
-        {
-            const last = its[ its.length - 1 ]
-            const rate = zhenfu( count )( its )
-            const minute = count * 3 / 60
-            const isUpDown = isUpDownConfig( count )
+//         data$?.pipe(
+//             bufferCount( count, 1 ),
+//         ).subscribe( its =>
+//         {
+//             const last = its[ its.length - 1 ]
+//             const rate = zhenfu( count )( its )
+//             const minute = count * 3 / 60
+//             const isUpDown = isUpDownConfig( count )
 
-            const time = last[ 0 ];
-            const rateStr = rate( it => it )?.toFixed( 2 ) ?? '';
-            const minuteStr = minute === 0.5 ? "半" : minute.toString();
-            const price = last[ 1 ];
-            const YiDongMsgCurryFac = curry( YiDongMsg )
-            const YiDongMsgCurry = YiDongMsgCurryFac( time )( codeName( code ) )( rateStr )( minuteStr )( price )
-            if ( isUpDown( fn.up )( its ) )
-            {
-                Log( YiDongMsgCurry( 'up' ) )
-                SendText( time, YiDongMsgCurry( 'up' ) )
-            }
-            if ( isUpDown( fn.down )( its ) )
-            {
-                Log( YiDongMsgCurry( 'down' ) )
-                SendText( time, YiDongMsgCurry( 'down' ) )
-            }
-        } )
-    }
-    const MinuteCount = 20
-    UpDown( MinuteCount * 0.5 )( {
-        up: rate => rate > 0.5,
-        down: rate => rate < -0.5
-    } )
-    UpDown( MinuteCount * 3 )( {
-        up: rate => rate > 1,
-        down: rate => rate < -1
-    } )
+//             const time = last[ 0 ];
+//             const rateStr = rate( it => it )?.toFixed( 2 ) ?? '';
+//             const minuteStr = minute === 0.5 ? "半" : minute.toString();
+//             const price = last[ 1 ];
+//             const YiDongMsgCurryFac = curry( YiDongMsg )
+//             const YiDongMsgCurry = YiDongMsgCurryFac( time )( codeName( code ) )( rateStr )( minuteStr )( price )
+//             if ( isUpDown( fn.up )( its ) )
+//             {
+//                 Log( YiDongMsgCurry( 'up' ) )
+//                 SendText( time, YiDongMsgCurry( 'up' ) )
+//             }
+//             if ( isUpDown( fn.down )( its ) )
+//             {
+//                 Log( YiDongMsgCurry( 'down' ) )
+//                 SendText( time, YiDongMsgCurry( 'down' ) )
+//             }
+//         } )
+//     }
+//     const MinuteCount = 20
+//     UpDown( MinuteCount * 0.5 )( {
+//         up: rate => rate > 0.5,
+//         down: rate => rate < -0.5
+//     } )
+//     UpDown( MinuteCount * 3 )( {
+//         up: rate => rate > 1,
+//         down: rate => rate < -1
+//     } )
 
-    data$?.subscribe( it =>
-    {
-        if ( isBig( it ) && it[ 4 ] !== "4" )
-        {
-            const time = it[ 0 ];
-            const price = it[ 1 ]
-            const money = formatMoney( it[ 1 ], it[ 2 ] );
-            const direction = it[ 4 ] === "2" ? 'up' : 'down';
+//     data$?.subscribe( it =>
+//     {
+//         if ( isBig( it ) && it[ 4 ] !== "4" )
+//         {
+//             const time = it[ 0 ];
+//             const price = it[ 1 ]
+//             const money = formatMoney( it[ 1 ], it[ 2 ] );
+//             const direction = it[ 4 ] === "2" ? 'up' : 'down';
 
-            const msg = DaDanMsg( time, codeName( code ), money, price, direction );
+//             const msg = DaDanMsg( time, codeName( code ), money, price, direction );
 
-            Log( msg )
-            SendText( time, msg )
-        }
-    } )
-} )
+//             Log( msg )
+//             SendText( time, msg )
+//         }
+//     } )
+// } )
 
 // 信息汇总
 /** 
@@ -171,31 +172,72 @@ tick$.map( it =>
     const { code, data$ } = it;
     const MinuteCount = 20
     data$?.pipe(
-        bufferCount( MinuteCount * 5 ),
-    ).subscribe( its =>
-    {
-        const last = its[ its.length - 1 ]
-        const first = its[0]
-        const time = `${first[0]} - ${last[ 0 ]}`;
-        const rate = Number(last[1]) - Number(first[1])
-        const percent = (rate / Number(first[1]) * 100)
-        const upCount = its.filter( it => it[ 4 ] === "2" ).reduce( ( p, v ) => p + Number( v[ 2 ] ), 0 );
-        const downCount = its.filter( it => it[ 4 ] === "1" ).reduce( ( p, v ) => p + Number( v[ 2 ] ), 0 );
-        const upBiShu = its.filter( it => it[ 4 ] === "2" ).reduce( ( p, v ) => p + Number( v[ 3 ] ), 0 );
-        const downBiShu = its.filter( it => it[ 4 ] === "1" ).reduce( ( p, v ) => p + Number( v[ 3 ] ), 0 );
-        const upMoney = its.filter( it => it[ 4 ] === "2" ).reduce( ( acc, cur ) => acc + parseFloat( cur[ 1 ] ) * parseInt( cur[ 2 ] ) / 100, 0 );
-        const downMoney = its.filter( it => it[ 4 ] === "1" ).reduce( ( acc, cur ) => acc + parseFloat( cur[ 1 ] ) * parseInt( cur[ 2 ] ) / 100, 0 );
-        const upAvg = ( upMoney / upBiShu ).toFixed( 2 );
-        const downAvg = ( downMoney / downBiShu ).toFixed( 2 );
-        const msg =
-            `
-时间：${ time }  代码：${ codeName( code ) }
-> 买入：${ upCount }手 ${ upBiShu }笔 ${ upAvg }万/笔 买入金额：${ upMoney.toFixed( 2 ) }万
-> 卖出：${ downCount }手 ${ downBiShu }笔 ${ downAvg }万/笔 卖出金额：${ downMoney.toFixed( 2 ) }万
-> 股价波动：${rate.toFixed(2)}(${percent.toFixed(2)}%) 买入/卖出：${ ( upMoney / downMoney ).toFixed( 2 ) }
-`
-        SendMD( last [0], msg, bot(code) )
-        Log( msg )
-    } )
+        // bufferTime(1000 * 60 )
+        bufferCount( MinuteCount * 1 ),
+        filter( its => its.length > 0 ),
+        map( its =>
+        {
+            const last = its[ its.length - 1 ]
+            const first = its[ 0 ]
+            const time = `${ first[ 0 ] } - ${ last[ 0 ] }`;
+            const rate = Number( last[ 1 ] ) - Number( first[ 1 ] )
+            const percent = ( rate / Number( first[ 1 ] ) * 100 )
+            const upCount = its.filter( it => it[ 4 ] === "2" ).reduce( ( p, v ) => p + Number( v[ 2 ] ), 0 );
+            const downCount = its.filter( it => it[ 4 ] === "1" ).reduce( ( p, v ) => p + Number( v[ 2 ] ), 0 );
+            const upBiShu = its.filter( it => it[ 4 ] === "2" ).reduce( ( p, v ) => p + Number( v[ 3 ] ), 0 );
+            const downBiShu = its.filter( it => it[ 4 ] === "1" ).reduce( ( p, v ) => p + Number( v[ 3 ] ), 0 );
+            const upMoney = its.filter( it => it[ 4 ] === "2" ).reduce( ( p, v ) => p + Number( v[ 1 ] ) * Number( v[ 2 ] ) / 100, 0 );
+            const downMoney = its.filter( it => it[ 4 ] === "1" ).reduce( ( p, v ) => p + Number( v[ 1 ] ) * Number( v[ 2 ] ) / 100, 0 );
+            const upAvg = upBiShu ? upCount / upBiShu : 0;
+            const downAvg = downBiShu ? downCount / downBiShu : 0;
+            const upMoneyAvg = upBiShu ? upMoney / upBiShu : 0;
+            const downMoneyAvg = downBiShu ? downMoney / downBiShu : 0;
+            return {
+                code,
+                time,
+                rate,
+                percent,
+                upCount,
+                downCount,
+                upBiShu,
+                downBiShu,
+                upMoney,
+                downMoney,
+                upAvg,
+                downAvg,
+                upMoneyAvg,
+                downMoneyAvg
+            }
+        } ),
+        bufferCount( 5 ),
+        map( its => ( {
+            time: `${ its[ 0 ]?.time } - ${ its[ its.length - 1 ]?.time }`,
+            rate: its.reduce( ( p, v ) => p + v.rate, 0 ),
+            upAvg: its.reduce( ( p, v ) => p + v.upAvg, 0 ),
+        } ) ),
+        map( it => ( {
+            time: it.time,
+            t: it.rate * it.upAvg * 100
+        } ) ),
+        map( it => ( {
+            time: it.time.split(" - ")[ it.time.split(" - ").length - 1 ],
+            t: it.t
+        })),
+        scan( ( p, v ) => {
+            p.time.push( v.time )
+            p.value.push( v.t )
+            return p
+        },{
+            time:Array<string>(),
+            value:Array<number>()
+        })
+    ).subscribe( Log )
 } )
 
+// const msg =
+// `
+// 时间：${ time }  代码：${ codeName( code ) }
+// > 买入：${ upCount }手 ${ upBiShu }笔 ${ upAvg }万/笔 买入金额：${ upMoney.toFixed( 2 ) }万
+// > 卖出：${ downCount }手 ${ downBiShu }笔 ${ downAvg }万/笔 卖出金额：${ downMoney.toFixed( 2 ) }万
+// > 股价波动：${rate.toFixed(2)}(${percent.toFixed(2)}%) 买入/卖出：${ ( upMoney / downMoney ).toFixed( 2 ) }
+// `
